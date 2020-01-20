76c3b237e435edd21249f614387d0da3f7c29378
Delaney Endean <delaneyendean@gmail.com>
2020-01-20 15:53:44 -0700

    Added the location for the user to know where to go in the lesson instructions

--- SwiftEV3-master copy/SwiftEV3/JoystickViewController.swift
+++ SwiftEV3-master copy/SwiftEV3/JoystickViewController.swift
@@ -31,7 +31,7 @@ class JoystickViewController: UIViewController {
             directionsText: "Geronimo the robot is stuck out on a bridge! Add the Move Forward Command 3 times to drive your robot out and save him.",
              simpleAnswer: [MoveForwardCommand.self, MoveForwardCommand.self, MoveForwardCommand.self]),
         Task(lessonNum: 2,
-        directionsText: " Geronimo the robot is a priate collecting treasure. Go and collect the missing pirate treasure.",
+        directionsText: " Geronimo the robot is a priate collecting treasure. Go and collect the missing pirate treasure located at E4 on the grid.",
         complexAnswer: ("E", 4)),
         Task(lessonNum: 3,
                directionsText: "Ants",
