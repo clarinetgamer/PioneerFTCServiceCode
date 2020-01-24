ddd1665f4541e9177d66ba3740bf9459df8bf0ad
Delaney Endean <delaneyendean@gmail.com>
2020-01-23 22:03:54 -0700

    Added the directions for all of the remaining lessons. Ready for presentation.

--- SwiftEV3-master copy/SwiftEV3/TasksDateSource.swift
+++ SwiftEV3-master copy/SwiftEV3/TasksDateSource.swift
@@ -40,28 +40,28 @@ struct TaskDateSource {
     
     static var lesson3 = Task(
         lessonNum: 3,
-        directionsText: " ",
+        directionsText: "Geronimo the robot is an ant who needs help finding his way back to his house at the ant hill. He starts at A5 on the grid. The ant hill is located at L3, L1, or L5. Please choose one point to help him find his way back home.",
         complexAnswer: [("L", 3), ("L", 1), ("L", 5)],
         position: Position(letterNum: 1, number: 5),
         showTaskButtons: ShowTaskButtons(showDirections: true, showSteppers: true, showArm: false))
     
     static var lesson3_1 = Task(
         lessonNum: 3.1,
-        directionsText: " ",
+        directionsText: "Geronimo the robot is an ant who needs help finding his way to the nursery. He currently is at L1 on the grid. The nursery is located at F4 or G4. Please choose one point to help him find his way to the nursery.",
         complexAnswer: [("F", 4), ("G", 4)],
         position: Position(letterNum: 12, number: 1),
         showTaskButtons: ShowTaskButtons(showDirections: true, showSteppers: true, showArm: false))
     
     static var lesson3_2 = Task(
         lessonNum: 3.2,
-        directionsText: " ",
+        directionsText: "Geronimo the robot is an ant who needs help finding his way to the Queen. The Queen is located at F2, F3, G2, or G3. He currently is at L1 on the grid.Please choose one point to help him find his way to the Queen",
         complexAnswer: [("F", 2), ("F", 3), ("G", 2), ("G", 3)],
         position: Position(letterNum: 12, number: 1),
         showTaskButtons: ShowTaskButtons(showDirections: true, showSteppers: true, showArm: true))
     
     static var lesson3_3 = Task(
         lessonNum: 3.3,
-        directionsText: " ",
+        directionsText: "Geronimo the robot is an ant who needs help finding his way to the farm. The farm is located at B4 or K4.  He currently is at L1 on the grid. Please choose one point to help him find his way to the farm ",
         complexAnswer: [("B", 4), ("K", 4)],
         position: Position(letterNum: 12, number: 1),
         showTaskButtons: ShowTaskButtons(showDirections: true, showSteppers: true, showArm: true))
